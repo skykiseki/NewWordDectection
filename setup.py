ed8bd129d556bdf0ea0d9f221a4a6729e9009ebb
skykiseki <595791833@qq.com>
2021-07-06 19:12:48 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+LONGDOC = """
+我家还蛮大的, 欢迎你们来我家van.
+
+https://github.com/skykiseki
+"""
+
+setup(name='new-words-detection',
+      version='1.0',
+      description='Chinese Words Segmentation Utilities',
+      long_description=LONGDOC,
+      author='Sun, Junyi',
+      author_email='ccnusjy@gmail.com',
+      url='https://github.com/fxsjy/jieba',
+      license="MIT",
+      classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Natural Language :: Chinese (Simplified)',
+        'Natural Language :: Chinese (Traditional)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Indexing',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+      keywords='NLP,Chinese word detection,Chinese word segementation',
+      packages=['new-words-detection'],
+      package_dir={'NewWordDetection':'new-words-detection'},
+      package_data={'new-words-detection':['*.*']}
+)
